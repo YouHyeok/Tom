389fbf9ba97068a5531513a7c0a615b56278cf25
enfycius <hexk0131@gmail.com>
2024-02-01 15:27:13 +0900

    Overriding an existing model

--- YouHyeok/Tom/models.py
+++ YouHyeok/Tom/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class SchedulingTasks(models.Model):
     user = models.CharField(max_length = 20, null=True)
     task = models.CharField(max_length = 20, null=True)
-    scheduled_id = models.IntegerField(null=True)
+    scheduled_id = models.CharField(max_length = 40, null=True)
     scheduled_time = models.IntegerField(null=True)
     created_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
