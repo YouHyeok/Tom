9f25239f3a355c9c00d5eab2917bce9a9dafdfcd
enfycius <hexk0131@gmail.com>
2024-02-01 10:45:08 +0900

    Using the TabView (Bottom Navigation Bar) to Split Views

---
+++ Tom/Tom/ScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleView.swift
+//  Tom
+//
+//  Created by 김종혁 on 2/1/24.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text("Schedule View")
+    }
+}
+
+struct ScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleView()
+    }
+}
