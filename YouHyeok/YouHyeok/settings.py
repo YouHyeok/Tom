e1991ee3e73845fa92fc07c4ee2fca23654411a3
enfycius <hexk0131@gmail.com>
2024-02-01 12:47:15 +0900

    Registering gitignore for the my_settings.py file and linking to the database

--- YouHyeok/YouHyeok/settings.py
+++ YouHyeok/YouHyeok/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import my_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,13 +81,7 @@ WSGI_APPLICATION = "YouHyeok.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
