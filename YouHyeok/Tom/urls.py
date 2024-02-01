6bbd18b414024dc4bc68d75370e8e23ae0ea4849
enfycius <hexk0131@gmail.com>
2024-02-01 21:31:26 +0900

    Define an endpoint

--- YouHyeok/Tom/urls.py
+++ YouHyeok/Tom/urls.py
@@ -9,5 +9,6 @@ import Tom.views
 urlpatterns = [
     path("get/categories", Tom.views.index, name='Get Categories'),
     path("get/scheduled_tasks", Tom.views.scheduled_tasks, name="Get Scheduled Tasks"),
-    path("trigger/upload/git", Tom.views.git, name="Trigger Uploading to GIT")
+    path("trigger/upload/git", Tom.views.git, name="Trigger Uploading to GIT"),
+    path("register/schedule", Tom.views.register_schedule, name="Registering the requested schedule")
 ] 
