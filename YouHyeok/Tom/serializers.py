9816be1128a304dcde35c3fdf3e3b8ba20a909fb
enfycius <hexk0131@gmail.com>
2024-02-01 15:27:30 +0900

    Overriding an existing serializer

--- YouHyeok/Tom/serializers.py
+++ YouHyeok/Tom/serializers.py
@@ -4,7 +4,7 @@ from Tom.models import SchedulingTasks
 class SchedulingSerializer(serializers.ModelSerializer):
     user = serializers.CharField(max_length = 20)
     task = serializers.CharField(max_length = 20)
-    scheduled_id = serializers.IntegerField()
+    scheduled_id = serializers.CharField(max_length = 40)
     scheduled_time = serializers.IntegerField()
 
     class Meta:
