2e4483affe440457efcf9639a07e9ea24a815712
enfycius <hexk0131@gmail.com>
2024-02-01 13:46:11 +0900

    Defining a Serializers

--- YouHyeok/Tom/serializers.py
+++ YouHyeok/Tom/serializers.py
@@ -1,1 +1,11 @@
 from rest_framework import serializers
+from Tom.models import SchedulingTasks
+
+class SchedulingSerializer(serializers.ModelSerializer):
+    task = serializers.CharField(max_length = 20)
+    scheduled_id = serializers.integerField()
+    scheduled_time = serializers.IntegerField()
+
+    class Meta:
+        model = SchedulingTasks
+        fields = ("task", "scheduled_id", "scheduled_time")
