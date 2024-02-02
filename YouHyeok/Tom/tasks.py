7ffdc30fcd10017aef34b0089a7090ecdeafb95a
enfycius <hexk0131@gmail.com>
2024-02-02 18:06:04 +0900

    fix #43

--- YouHyeok/Tom/tasks.py
+++ YouHyeok/Tom/tasks.py
@@ -4,6 +4,8 @@ import json
 from django.conf import settings
 from celery import shared_task
 
+from celery.exceptions import Ignore
+
 users_repo = {"Jong": ["_tutorials", "_labs", "_enfycius"], "You": ["newblog"]}
 
 @shared_task()
@@ -12,9 +14,17 @@ def git_task(user, task):
     category = task.split('-')[1]
     repo = task.split('-')[0]
 
-    if repo in repos:
-        with open('/root/Repos/%s/package.json' % repo) as f:
-            json_object = json.load(f)
+    try:
+        if repo in repos:
+            with open('/root/Repos/%s/package.json' % repo) as f:
+                json_object = json.load(f)
 
-            os.system("cd /root/Repos/%s/ && git config --local user.name \"%s\" && git config --local user.email \"%s\"" % (repo, json_object['scripts']['user_name'], json_object['scripts']['user_email']))
-            os.system("cd /root/Repos/%s/ && npm run %s" % (repo, category))
+                os.system("cd /root/Repos/%s/ && git config --local user.name \"%s\" && git config --local user.email \"%s\"" % (repo, json_object['scripts']['user_name'], json_object['scripts']['user_email']))
+                os.system("cd /root/Repos/%s/ && npm run %s" % (repo, category))
+
+        raise Ignore
+    
+    except Ignore:
+        pass
+
+    
