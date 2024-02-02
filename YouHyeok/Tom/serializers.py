df40dcc27f116196b584004fc8a70f72e2003deb
enfycius <hexk0131@gmail.com>
2024-02-02 19:00:01 +0900

    fix #46

--- YouHyeok/Tom/serializers.py
+++ YouHyeok/Tom/serializers.py
@@ -4,7 +4,7 @@ from Tom.models import SchedulingTasks
 class SchedulingSerializer(serializers.ModelSerializer):
     user = serializers.CharField(max_length = 20)
     task = serializers.CharField(max_length = 20)
-    scheduled_id = serializers.CharField(max_length = 40)
+    scheduled_id = serializers.CharField(max_length = 40, allow_blank=True, allow_null=True)
     scheduled_time = serializers.IntegerField()
 
     class Meta:
